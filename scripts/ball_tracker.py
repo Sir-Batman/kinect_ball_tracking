41ab6648f675380e8ae00880c9159d84fee60e26
Connor Yates <yatesco@oregonstate.edu>
2018-04-17 17:51:24 -0700

    Some parameter tuning on thecircle detection, can be visualized  via imgshow. Doesn't actually find the ball in the image though...

--- scripts/ball_tracker.py
+++ scripts/ball_tracker.py
@@ -9,7 +9,7 @@ from sensor_msgs.msg import Image
 class BallTracker(object):
 	def __init__(self):
 		self.pub = rospy.Publisher("/ball_pos", Point, queue_size=10)
-		self.im_pub = rospy.Publisher("ball_highlight_image", Image, queue_size=10)
+		self.im_pub = rospy.Publisher("/ball_highlight_image", Image, queue_size=10)
 		self.sub = rospy.Subscriber("/camera/depth/image", Image, self.process_image)
 		self.bridge = CvBridge()
 	def process_image(self, image):
@@ -21,28 +21,31 @@ class BallTracker(object):
 		(rows,cols) = cv_img.shape
 		print "try 1"
 		cv_img = cv2.medianBlur(cv_img, 5)
-		cv2.imshow("img",cv_img)
-		cv2.waitKey(1)
+		#cv2.imshow("img",cv_img)
+		#cv2.waitKey(1)
 		print "try 2"
 		cimg = cv2.cvtColor(cv_img, cv2.COLOR_GRAY2BGR)
+		print cimg.shape
 		print "try 3"
 		cv_img = np.nan_to_num(cv_img)
 		print cv_img
 		print "MAX:", np.max(cv_img)
 		print "MIN:", np.min(cv_img)
 		print "TYPE:", type(cv_img)
-		circles = cv2.HoughCircles(cv_img.astype(int), cv2.HOUGH_GRADIENT, 1, 20, param1=50, param2=30, minRadius=0, maxRadius=0)
+		cv_img *= 255.0/cv_img.max()
+		cv_img = np.clip(cv_img, 0, 255)
+		circles = cv2.HoughCircles(cv_img.astype(np.uint8), cv2.HOUGH_GRADIENT, 2, 50, param1=30, param2=80, minRadius=20, maxRadius=130)
 		print "try 4"
 		circles = np.uint16(np.around(circles))
 		print "try 5"
 		for i in circles[0,:]:
 			cv2.circle(cimg, (i[0],i[1]),i[2],(0,255,0),2)
 			cv2.circle(cimg, (i[0],i[1]),2,(0,0,255),3)
-		
-		# Republishes the image as a color image with the ball highlighted.
-		# As a debug feature, this should be set via a roslaunch param
 		try:
-			self.im_pub.publish(self.bridge.cv2_to_imgmsg(color_im))
+			cv2.imshow("circles on image", cimg)
+			cv2.waitKey(1)
+			new_msg_img = self.bridge.cv2_to_imgmsg(cimg, "32FC3")
+			self.im_pub.publish(new_msg_img)
 		except CvBridgeError as e:
 			print e
 		
